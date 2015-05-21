424ad382070671c81062b148b513ed2989113efa
Chris Bastian <peacethrubeats@gmail.com>
2015-05-21 12:29:29 -0700

    controllers and models finished

---
+++ app/controllers/admin/categories_controller.rb
@@ -0,0 +1,22 @@
+class Admin::CategoriesController < ApplicationController
+  def new
+  end
+
+  def create
+  end
+
+  def edit
+  end
+
+  def update
+  end
+
+  def destory
+  end
+
+  def index
+  end
+
+  def show
+  end
+end
---
+++ app/controllers/admin/users_controller.rb
@@ -0,0 +1,25 @@
+class Admin::UsersController < ApplicationController
+  def new
+  end
+
+  def create
+  end
+
+  def edit
+  end
+
+  def update
+  end
+
+  def delete
+  end
+
+  def destroy
+  end
+
+  def index
+  end
+
+  def show
+  end
+end
