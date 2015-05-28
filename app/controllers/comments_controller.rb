13ecefb4af0806abf1c0f748a83d1a50ca8099b3
Chris Bastian <peacethrubeats@gmail.com>
2015-05-27 23:45:13 -0700

    front end controllers

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,4 +1,17 @@
 class CommentsController < ApplicationController
-  def new
+
+  def create
+    @post = Post.find(params[:post_id])
+    @comment = @post.comments.create(comment_params)
+
+    flash[:notice] = "Comment Addded"
+
+    redirect_to post_path(@post)
   end
+
+  private
+    def comment_params
+      params.require(:comment).permit(:name, :email, :body, :post_id)
+    end
+
 end
